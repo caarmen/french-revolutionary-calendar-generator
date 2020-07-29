276224eb35a37f77cc6d9e08a609e74210d77ecc
Carmen Alvarez <carmen@rmen.ca>
2020-07-29 16:01:08 +0200

    Add copyright notice and headers

--- src/main/kotlin/ca/rmen/frcg/ArgumentParser.kt
+++ src/main/kotlin/ca/rmen/frcg/ArgumentParser.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020 Carmen Alvarez
+ *
+ * This file is part of French Revolutionary Calendar Generator
+ * French Revolutionary Calendar Generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * French Revolutionary Calendar Generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with French Revolutionary Calendar Generator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ca.rmen.frcg
 
 import kotlinx.cli.ArgParser
